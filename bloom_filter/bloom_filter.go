a026e39bb3930e0f65ff0b407289fd06bff599bb
urosh2003 <radukic.uros@gmail.com>
2024-01-31 14:08:51 +0100

    updated bloom filter and sstable

--- bloom_filter/bloom_filter.go
+++ bloom_filter/bloom_filter.go
@@ -2,49 +2,35 @@ package bloom_filter
 
 import (
 	"bytes"
-	"encoding/binary"
+	"encoding/gob"
 	"hash/fnv"
+	"io"
 	"os"
 )
 
 type BloomFilter struct {
-	size     int
-	hashFunc []func(string) int
-	bits     []bool
+	Size         int // promenjeno Size
+	NumHashFuncs int
+	HashSeeds    []int
+	Bits         []bool
 }
 
 func NewBloomFilter(size int, numHashFuncs int) *BloomFilter {
 	bloom := BloomFilter{
-		size:     size,
-		hashFunc: make([]func(string) int, numHashFuncs),
-		bits:     make([]bool, size),
+		Size:         size,
+		NumHashFuncs: numHashFuncs,
+		HashSeeds:    make([]int, numHashFuncs),
+		Bits:         make([]bool, size),
 	}
 
 	for i := 0; i < numHashFuncs; i++ {
-		bloom.hashFunc[i] = createHashFunc(i)
+		bloom.HashSeeds[i] = i
 	}
 
 	return &bloom
 }
 
-func (b *BloomFilter) Add(value string) {
-	for _, hashFunc := range b.hashFunc {
-		index := hashFunc(value) % b.size
-		b.bits[index] = true
-	}
-}
-
-func (b *BloomFilter) Contains(value string) bool {
-	for _, hashFunc := range b.hashFunc {
-		index := hashFunc(value) % b.size
-		if !b.bits[index] {
-			return false
-		}
-	}
-	return true
-}
-
-func createHashFunc(seed int) func(string) int {
+func (b *BloomFilter) createHashFunc(seed int) func(string) int {
 	return func(data string) int {
 		hash := fnv.New32a()
 		hash.Write([]byte(data))
@@ -52,47 +38,97 @@ func createHashFunc(seed int) func(string) int {
 	}
 }
 
-// MarshalBinary implements the encoding.BinaryMarshaler interface.
-func (b *BloomFilter) MarshalBinary() ([]byte, error) {
-	var buf bytes.Buffer
+func (b *BloomFilter) Add(value string) {
+	for _, seed := range b.HashSeeds {
+		index := b.createHashFunc(seed)(value) % b.Size
+		b.Bits[index] = true
+	}
+}
 
-	// Write size to the buffer
-	if err := binary.Write(&buf, binary.BigEndian, int32(b.size)); err != nil {
+func (b *BloomFilter) Contains(value string) bool {
+	for _, seed := range b.HashSeeds {
+		index := b.createHashFunc(seed)(value) % b.Size
+		if !b.Bits[index] {
+			return false
+		}
+	}
+	return true
+}
+
+// Serijalizacija BloomFilter-a u bajt niz koristeći gob
+func (b *BloomFilter) Serialize() ([]byte, error) {
+	var data bytes.Buffer
+	encoder := gob.NewEncoder(&data)
+
+	err := encoder.Encode(b)
+	if err != nil {
 		return nil, err
 	}
 
-	// Write bits to the buffer
-	for _, bit := range b.bits {
-		if err := binary.Write(&buf, binary.BigEndian, bit); err != nil {
-			return nil, err
-		}
-	}
-
-	return buf.Bytes(), nil
+	return data.Bytes(), nil
 }
 
-// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface.
-func (b *BloomFilter) UnmarshalBinary(data []byte) error {
-	buf := bytes.NewBuffer(data)
+// Deserijalizacija BloomFilter-a iz bajt niza koristeći gob
+func Deserialize(data []byte) (*BloomFilter, error) {
+	var bloom BloomFilter
+	buffer := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(buffer)
 
-	// Read size from the buffer
-	var size int32
-	if err := binary.Read(buf, binary.BigEndian, &size); err != nil {
+	err := decoder.Decode(&bloom)
+	if err != nil {
+		return nil, err
+	}
+
+	return &bloom, nil
+}
+
+// Čuvanje BloomFilter-a u datoteku koristeći gob
+func (b *BloomFilter) SaveToFile(filename string) error {
+	data, err := b.Serialize()
+	if err != nil {
 		return err
 	}
-	b.size = int(size)
 
-	// Read bits from the buffer
-	b.bits = make([]bool, b.size)
-	for i := 0; i < b.size; i++ {
-		var bit bool
-		if err := binary.Read(buf, binary.BigEndian, &bit); err != nil {
-			return err
-		}
-		b.bits[i] = bit
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+	return err
+}
+
+// Učitavanje BloomFilter-a iz datoteke koristeći gob
+func LoadFromFile(filename string) (*BloomFilter, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
 	}
 
-	return nil
+	return Deserialize(data)
+}
+
+/*
+func main() {
+	bloomFilter := NewBloomFilter(100, 3)
+	bloomFilter.Add("element1")
+	bloomFilter.Add("element2")
+	bloomFilter.Add("element3")
+
+	// Čuvanje BloomFilter-a u datoteku
+	err := bloomFilter.SaveToFile("data.gob")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("BloomFilter je uspešno sačuvan u datoteku.")
 }
 
 func ReadBloomFilter(filename string) (bf *BloomFilter) {
